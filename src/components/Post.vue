fdc883b5dc4745184b881fac0acb99a89472b609
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-04-23 17:32:53 -0600

    end of day commit

---
+++ src/components/Post.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="post-photo col-md-3">
+    <div class=" position-relative">
+      <img :src="post.imgUrl" alt="Post Image">
+      <div class="text-left">
+        <h4>{{ post.body }}</h4>
+      </div>
+      <!-- <router-link :to="{name: 'ProjectDetails', params: {id: post.id}}">
+        <img :src="post.imgUrl" alt="Project Cover Image">
+        <img class="rounded-circle small-img position-absolute" :src="post.creator.picture" alt="Creator Photo">
+      </router-link> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ img{
+   width: 100%
+ }
+
+ .small-img{
+   width: 1em;
+   left: 0;
+   top: 5px
+ }
+</style>
--- src/router.js
+++ src/router.js
@@ -21,6 +21,12 @@ const routes = [
     name: 'Account',
     component: loadPage('AccountPage'),
     beforeEnter: authGuard
+  },
+  {
+    path: '/post/:id',
+    name: 'PostDetails',
+    component: loadPage('PostDetailsPage')
+
   }
 ]
 
